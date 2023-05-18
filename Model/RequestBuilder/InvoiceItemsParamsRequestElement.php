878e3ec0441c49133f8063a84052408dea21e91f
Eduard Karnaukh <ed@magesoft.co>
2023-05-18 10:50:04 -0400

    price incl tax in invoice

--- Model/RequestBuilder/InvoiceItemsParamsRequestElement.php
+++ Model/RequestBuilder/InvoiceItemsParamsRequestElement.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 namespace TradeCentric\Invoice\Model\RequestBuilder;
 
 use Magento\Sales\Api\Data\InvoiceInterface;
+use Magento\Sales\Api\Data\InvoiceItemInterface;
 use TradeCentric\Invoice\Api\RequestBuildElementInterface;
 use TradeCentric\Invoice\Api\RequestBuilderInterface;
+use Magento\Weee\Block\Item\Price\Renderer;
 
 /**
  * Class ParamsRequestElement
@@ -13,6 +15,15 @@ use TradeCentric\Invoice\Api\RequestBuilderInterface;
  */
 class InvoiceItemsParamsRequestElement implements RequestBuildElementInterface
 {
+    /** @var Renderer */
+    protected $renderer;
+
+    public function __construct(
+        Renderer $renderer
+    ) {
+        $this->renderer = $renderer;
+    }
+
     /**
      * @param RequestBuilderInterface $requestBuilder
      * @param InvoiceInterface $invoice
@@ -20,22 +31,26 @@ class InvoiceItemsParamsRequestElement implements RequestBuildElementInterface
     public function build(RequestBuilderInterface $requestBuilder, InvoiceInterface $invoice): void
     {
         $result = [];
+        /** @var InvoiceItemInterface $invoiceItem */
         foreach ($invoice->getAllItems() as $invoiceItem) {
             $orderItem = $invoiceItem->getOrderItem();
             if (!$orderItem || $orderItem->getParentItemId()) {
                 continue;
             }
+
+            $this->renderer->setItem($invoiceItem);
+
             $result[] = array(
                 'line_number' => $orderItem->getExtensionAttributes()->getLineNumber(),
                 'quantity' => $invoiceItem->getQty() ,
                 'part_id' => $invoiceItem->getSku(),
                 'aux_part_id' => '',
                 'description' => $invoiceItem->getName(),
-                'price' => $invoiceItem->getPrice(),
-                'subtotal' => $invoiceItem->getRowTotal(),
+                'price' => $this->renderer->getUnitDisplayPriceExclTax(),
+                'subtotal' => $this->renderer->getRowDisplayPriceExclTax(),
                 'tax' => $invoiceItem->getTaxAmount(),
                 'discount' => $invoiceItem->getDiscountAmount(),
-                'row_total' => $invoiceItem->getRowTotalInclTax()
+                'row_total' => $this->renderer->getRowDisplayPriceInclTax()
             );
         }
         $requestBuilder->addParams([
